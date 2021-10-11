8d30eb088386df84c10dc1e0ee2461ebc1714b3b
lana rodion <lana.rodion@gmail.com>
2021-10-11 16:55:45 +0200

    Added import FormsModule and userInput

--- ngApp/src/app/app.module.ts
+++ ngApp/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -20,7 +21,8 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
